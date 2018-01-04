86b248e7d711a89df407a02e55de3b4bd530fc70
Oskar Järviniemi <oskar.jarviniemi@hotmail.com>
2018-01-04 16:21:19 +0100

    added database and connected it

--- app/config.php
+++ app/config.php
@@ -6,5 +6,5 @@ declare(strict_types=1);
 
 return [
     'title' => 'Cyber-Link',
-    'database_path' => sprintf('sqlite:%s/database/database.db', __DIR__),
+    'database_path' => sprintf('sqlite:%s/database/users.db', __DIR__),
 ];
