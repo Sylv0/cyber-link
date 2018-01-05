64bbd9fda320be3819cb5d5efdd10ea0046db141
Oskar Järviniemi <oskar.jarviniemi@hotmail.com>
2018-01-05 15:53:30 +0100

    changed few minor stuff and added a post table

--- app/auth/createUser.php
+++ app/auth/createUser.php
@@ -6,10 +6,10 @@ require __DIR__.'/../autoload.php';
 
 if(isset($_POST['username']) && isset($_POST['password'])) {
 
-  $user = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
+  $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
   $email = filter_var($_POST['email'],FILTER_SANITIZE_EMAIL);
   $password = filter_var($_POST['password'], FILTER_SANITIZE_STRING);
 
-  $statement = $pdo->query("INSERT INTO 'user'(name, password, email) VALUES ('$user', '$password', '$email')");
+  $statement = $pdo->query("INSERT INTO 'user'(name, password, email) VALUES ('$username', '$password', '$email')");
   redirect('../../login.php');
 }
