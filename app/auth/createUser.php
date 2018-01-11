7077520de0fe398fb032426e718ca3093d9da5ae
Oskar Järviniemi <oskar.jarviniemi@hotmail.com>
2018-01-11 15:13:31 +0100

    stuffff

--- app/auth/createUser.php
+++ app/auth/createUser.php
@@ -10,6 +10,13 @@ if(isset($_POST['username']) && isset($_POST['password'])) {
   $email = filter_var($_POST['email'],FILTER_SANITIZE_EMAIL);
   $password = password_hash(filter_var($_POST['password'], FILTER_SANITIZE_STRING),PASSWORD_DEFAULT);
 
-  $statement = $pdo->query("INSERT INTO 'user'(name, password, email) VALUES ('$username', '$password', '$email')");
+  $statement = $pdo->prepare("INSERT INTO user(name, password, email) VALUES (:username, :password, :email)");
+
+  $statement ->bindParam(':email', $email);
+  $statement ->bindParam(':username', $username);
+  $statement ->bindParam(':password', $password);
+
+  $statement->execute();
+
   redirect('../../login.php');
 }
