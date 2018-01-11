8ad1801247fc4904c31b2dfa05bd64487e3db576
Oskar Järviniemi <oskar.jarviniemi@hotmail.com>
2018-01-11 09:03:13 +0100

    even more stuff

--- index.php
+++ index.php
@@ -1,32 +1,42 @@
 <?php require __DIR__.'/views/header.php'; ?>
 
 <article>
-    <h1><?php echo $config['title']; ?></h1>
-    <p>This is the home page.</p>
+  <h1><?php echo $config['title']; ?></h1>
+  <p>This is the home page.</p>
+
 </article>
-<?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-    echo "Welcome " . $_SESSION['user'] . "!";
- ?>
+<?php if (isset($_SESSION['userId'])) {
+  //$statement = $pdo->query("SELECT name FROM user WHERE userId = '$_SESSION['user']' ");
+
+  $statement = $pdo->prepare('SELECT name FROM user WHERE userId = :name');
+  $statement ->bindParam(':name', $_SESSION['userId']);
+
+  $statement->execute();
+
+  $user = $statement->fetch(PDO::FETCH_ASSOC);
+
+  echo "Welcome " .$user['name']. "!"
+
+  ?>
   <article>
-      <h1>Post</h1>
+    <h1>Post</h1>
 
-      <form action="app/posts/store.php" method="post">
-          <div class="form-group">
-              <label for="Title">Title</label>
-              <input class="form-control" type="text" name="title" placeholder="Title" required>
-          </div><!-- /form-group -->
+    <form action="app/posts/store.php" method="post">
+      <div class="form-group">
+        <label for="title">Title</label>
+        <input class="form-control" type="text" name="title" placeholder="Title" required>
+      </div><!-- /form-group -->
 
-          <div class="form-group">
-              <label for="context">Text</label>
-              <input class="form-control" type="text" name="context" required>
+      <div class="form-group">
+        <label for="content">Text</label>
+        <input class="form-control" type="text" name="content" required>
+      </div><!-- /form-group -->
 
-          </div><!-- /form-group -->
-
-          <button type="submit" class="btn btn-primary">Submit</button>
-      </form>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
   </article>
 <?php }else {
-      echo "Please log in first to see this page.";
-  } ?>
+  echo "Please log in first to see this page.";
+} ?>
 
 <?php require __DIR__.'/views/footer.php'; ?>
