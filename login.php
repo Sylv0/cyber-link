6407665bce0dca74b692c6b00a7cbe7756a9d46d
Oskar Järviniemi <oskar.jarviniemi@hotmail.com>
2018-01-14 23:01:46 +0100

    Changed some layout, essentially the same thing

--- login.php
+++ login.php
@@ -1,23 +1,26 @@
 <?php require __DIR__.'/views/header.php'; ?>
 
 <article>
-    <h1>Login</h1>
+  <h1>Login</h1>
 
-    <form action="app/auth/login.php" method="post">
-        <div class="form-group">
-            <label for="email">Email</label>
-            <input class="form-control" type="email" name="email" required>
-            <small class="form-text text-muted">Please provide your email address.</small>
-        </div><!-- /form-group -->
+  <form action="app/auth/login.php" method="post">
+    <div class="form-group">
+      <label for="email">Email</label>
+      <input class="form-control" type="email" name="email" required>
+      <small class="form-text text-muted">Please provide your email address.</small>
+    </div><!-- /form-group -->
 
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input class="form-control" type="password" name="password" required>
-            <small class="form-text text-muted">Please provide your password (passphrase).</small>
-        </div><!-- /form-group -->
+    <div class="form-group">
+      <label for="password">Password</label>
+      <input class="form-control" type="password" name="password" required>
+      <small class="form-text text-muted">Please provide your password (passphrase).</small>
+    </div><!-- /form-group -->
 
-        <button type="submit" class="btn btn-primary">Login</button>
-    </form>
+    <button type="submit" class="btn btn-primary">Sign in</button>
+  </form>
+  <div class="dropdown-divider"></div>
+  <a class="dropdown-item" href="./createUser.php">New around here? Sign up</a>
 </article>
+
 
 <?php require __DIR__.'/views/footer.php'; ?>
