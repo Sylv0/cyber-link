d6826dc760c5ee27ee603f46bf82d8373f7ffefe
Oskar Järviniemi <oskar.jarviniemi@hotmail.com>
2018-01-14 23:01:57 +0100

    Tried to do stuff, didnt work

--- views/navigation.php
+++ views/navigation.php
@@ -1,26 +1,48 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#"><?php echo $config['title']; ?></a>
-
-  <ul class="navbar-nav">
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav">
       <li class="nav-item">
-          <a class="nav-link" href="./index.php">Home</a>
+        <a class="nav-link" href="./index.php">Home</a>
       </li><!-- /nav-item -->
 
       <li class="nav-item">
-          <a class="nav-link" href="./about.php">About</a>
+        <a class="nav-link" href="./about.php">About</a>
       </li><!-- /nav-item -->
-      <li class="nav-item">
-        <?php if (!isset($_SESSION['userId'])){ ?>
+    </ul>
+
+    <ul class ="navbar-nav right">
+      <?php if (!isset($_SESSION['userId'])){ ?>
+
+        <li class="nav-item">
           <a class="nav-link" href="./login.php">Login</a>
         </li>
-          <li class="nav-item">
-            <a class="nav-link" href="./createUser.php">Register</a>
-          </li><!-- /nav-item -->
-        <?php } else { ?>
-          <a class="nav-link" href="app/auth/logout.php">Log Out</a>
 
-    <?php } ?>
+        <li class="nav-item">
+          <a class="nav-link" href="./createUser.php">Sign up</a>
+        </li><!-- /nav-item -->
 
+      <?php } else { ?>
 
-  </ul><!-- /navbar-nav -->
+        <li class="nav-item">
+          <a class="nav-link" href="app/posts/store.php">Log out</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link" href="./profile.php">Profile</a>
+        </li>
+        <!--
+        <div class="dropdown">
+          <button class="dropbtn" onclick="dropDown()">
+            <?php echo $_SESSION['userId'] ?>
+          </button>
+          <div class="dropdown-content" id="myDropdown">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
+        </div>-->
+      <?php } ?>
+    </ul>
+  </div>
 </nav><!-- /navbar -->
