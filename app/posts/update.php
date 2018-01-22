6d7899ece94d6356eff79a3bb3c9f34d7551f22c
Oskar Järviniemi <oskar.jarviniemi@hotmail.com>
2018-01-22 09:28:32 +0100

    shit that doesnt work

--- app/posts/update.php
+++ app/posts/update.php
@@ -4,6 +4,19 @@ declare(strict_types=1);
 
 require __DIR__.'/../autoload.php';
 
-// In this file we delete posts in the database.
 
-redirect('/');
+if(isset($_POST['title']) && isset($_POST['content']))
+{
+  $title = filter_var($_POST['title'], FILTER_SANITIZE_STRING);
+  $content = filter_var($_POST['content'], FILTER_SANITIZE_STRING);
+  $postId = $_POST['postId'];
+
+  $updatePost = $pdo->prepare("UPDATE posts SET title= :title, content= :content WHERE postId= :id");
+
+  $updatePost ->bindParam(':id', $postId);
+  $updatePost ->bindParam(':username', $username);
+
+  $updatePost->execute();
+  redirect('../../profile.php');
+
+}
